c52bde6818860c6637d4faced36994a08055d83c
= <=>
2018-05-09 21:45:27 +0530

    Supply results and Exam wise result

--- app.R
+++ app.R
@@ -1,27 +1,15 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 source('results.R')
-# Define UI for application that draws a histogram
 ui <- fluidPage(
    
-   # Application title
-   titlePanel("Grade Analysis"),
+  titlePanel("Grade Analysis"),
    
-   # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
          textInput(inputId = 'registrationNumber',label = 'Registration Number',value = 'MDL16CS001')
       ),
       
-      # Show a plot of the generated distribution
+
       mainPanel(
          tabsetPanel(
            tabPanel('Grades',dataTableOutput('Grades')),
@@ -32,12 +20,12 @@ ui <- fluidPage(
    )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
    
    output$Grades <- renderDataTable({arrange(subset(subset(result,Reg_no == input$registrationNumber),select = -Reg_no
    ),Subject)})
-   output$Frequency <- renderPlot({barplot(table(subset(result,Reg_no == input$registrationNumber)$Grade),xlab="Grades",ylab="Frequency")})
+   #a <- data.frame(table(subset(result,Reg_no == input$registrationNumber)$Grade))
+   output$Frequency <- renderPlot({ggplot(subset(result,Reg_no == input$registrationNumber),aes(Grade))+geom_bar(aes(fill=Exam))+ylab("Frequency")+theme_minimal()})
    output$Course <- renderDataTable({courseNames})
    }
 
--- results.R
+++ results.R
@@ -7,29 +7,41 @@ gradify <- function(a){ return(unlist(strsplit(a,split="_",fixed=TRUE)[2])) }
 s1<-read.csv("S1.csv",stringsAsFactors = FALSE)
 s2<-read.csv("S2.csv",stringsAsFactors = FALSE) 
 s3<-read.csv("S3.csv",stringsAsFactors = FALSE)
+supply<- read.csv("supply.csv",stringsAsFactors = TRUE)
+supply <-subset(supply,Reg_no !='\f')
 courseNames <- read.csv("course_code.csv")
-totalResult <- merge(s1,s2)
-totalResult <- merge(totalResult,s3)
+s1 <- melt(s1 , id.vars ="Reg_no")
+s1$Sem <-'S1'
+s2 <- melt(s2 , id.vars ="Reg_no")
+s2$Sem <-'S2'
+s3 <- melt(s3 , id.vars ="Reg_no")
+s3$Sem <-'S3'
+b <- supply %>%gather(key,value,-Reg_no)
+b<- subset(b,value!="")
+supply <- b
+supply$sem <- 'Supply.'
+names(supply)<-c('Reg_no','variable','value','Sem')
 #barplot(table(subset(result,Reg_no == 'MDL16CS053')$Grade),xlab="Grade",ylab="Frequency",main="Grade Chart for MDL16CS053")
-result <- melt(totalResult,id.vars = "Reg_no")
+result <- rbind(s1,s2)
+result <- rbind(result,s3)
+
 a <- lapply(result$value,str_replace,pattern = '\\(',replacement = '_')
 a <- lapply(a,str_replace,pattern = '\\)',replacement = '')
 a<-str_split_fixed(a,"_",2)
+b <- lapply(supply$value,str_replace,pattern = '\\(',replacement = '_')
+b <- lapply(b,str_replace,pattern = '\\)',replacement = '')
+b <- str_split_fixed(b,'_',2)
+
 result$variable <- a[,1]
 result$value <- a[,2]
-
-names(result) <- c('Reg_no','Subject','Grade')
-MA101 <- data.frame(table(totalResult$Subject1))
-Grade <- c('A','A+','Absent','B','B+','C','F','O')
-names(MA101) <- c("Grade","Frequency")
-MA101$Grade <- Grade
-
-#pie(MA101$Frequency,MA101$Grade,main="Calculus result of S1 for year 2016-17")
-MA1012 <- data.frame(table(totalResult$Subject17))
-Grade <- c('A','A+','B','B+','C','F','O','P')
-names(MA1012) <- c("Grade","Frequency")
-
-MA1012$Grade <- Grade
-MA201 <- data.frame(table(totalResult$Subject19))
-names(MA201) <- c('Grade','Frequency')
-MA201$Grade <- c('A','A+','Absent','B','B+','C','F','FE','O','P')
+supply$variable <- b[,1]
+supply$value <- b[,2]
+names(supply) <- c('Reg_no','Subject','Grade','Exam')
+names(result) <- c('Reg_no','Subject','Grade','Exam')
+result2 <- subset(result,Exam =='S3')
+result <- subset(result,Exam !='S3')
+result <- subset(result,Grade !='F')
+result <- subset(result,Grade !='FE')
+result <- subset(result,Grade !='Absent')
+result <-rbind(result,supply)
+result <- rbind(result,result2)
